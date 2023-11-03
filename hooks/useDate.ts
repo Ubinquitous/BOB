0bcac47a9711a797967e71f7cc95ebe75dc981c1
Ubinquitous <ubinquitous1@gmail.com>
2023-11-03 11:31:32 +0900

    fix : 급식 시간 오류

--- hooks/useDate.ts
+++ hooks/useDate.ts
@@ -1,8 +1,12 @@
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import "dayjs/locale/ko";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
 dayjs.extend(customParseFormat).locale("ko");
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const useDate = () => {
   const date = new Date();
@@ -13,18 +17,23 @@ const useDate = () => {
   };
 
   const getCurrentDate = (): string => {
-    const date = dayjs();
+    const date = dayjs().utc().tz("Asia/Seoul");
+
     if (date.hour() >= 19) date.add(1, "day");
 
     return date.format("YYYYMMDD");
   };
 
   const getDayOfWeek = (date: string) => {
-    return dayjs(date, "YYMMDD").locale("ko").format("dddd");
+    return dayjs(date, "YYMMDD")
+      .utc()
+      .tz("Asia/Seoul")
+      .locale("ko")
+      .format("dddd");
   };
 
   const setDay = (day: number, date: string) => {
-    const dateObj = dayjs(date, { format: "YYYYMMDD" });
+    const dateObj = dayjs(date, { format: "YYYYMMDD" }).utc().tz("Asia/Seoul");
     return dateObj.add(day, "day").format("YYYYMMDD");
   };
 
